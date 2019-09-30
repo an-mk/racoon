8e71057225553be486925f9ea712103570c7112d
misztsu <adam.pawlowski.237@gmail.com>
2019-09-30 14:29:53 +0200

    Files in database working

---
+++ models/File.js
@@ -0,0 +1,55 @@
+const { createModel } = require('mongoose-gridfs');
+const db = require('../db')
+const fs = require('fs')
+
+var File = undefined
+async function getFile() {
+    if (File === undefined) {
+        create = function () {
+            File = createModel({
+                modelName: 'File'
+            })
+        }
+        if (db.connection.readyState == 1)
+            create()
+        else 
+            mongoose.connection.on('connected', create);
+    }
+    return File
+}
+
+async function write(filename, fileStream) {
+    await getFile()
+    return await new Promise((resolve, reject) => {
+        File.write({ filename: filename, contentType: 'text/plain' }, fileStream, async (error, file) => {
+            if(error) {
+                reject(error)
+            }
+            else 
+                resolve(file)
+        })
+    })
+}
+
+async function getStream(fileId) {
+    await getFile()
+    return await File.read({ _id: fileId })
+}
+
+async function toFile(fileId, path) {
+    const writeStream = fs.createWriteStream(path)
+    const readStream = await getStream(fileId)
+    await new Promise(resolve => {
+        writeStream.on('finish', resolve)
+        readStream.pipe(writeStream)
+    })
+}
+
+async function unlink(fileId) {
+    await getFile()
+    await File.unlink({ fileId }, (err) => {
+        throw err
+    })
+}
+
+module.exports = { write, getStream, toFile, unlink, getFile }
--- tests.js
+++ tests.js
@@ -1,42 +1,37 @@
 const Problem = require('./models/Problem')
 const Test = require('./models/Test')
+const File = require('./models/File')
 
 const { createModel } = require('mongoose-gridfs');
 
 async function insertTest(problem, fileStream) {
-    const TestFile = await createModel({
-        modelName: 'TestFiles'
+    file = await File.write('test_' + problem, fileStream).catch(err => {
+        console.log('test upload to database error')
+        throw err
     })
-    await TestFile.write({ filename: 'test_' + problem, contentType: 'text/plain' }, fileStream, async (error, file) => {
-        if(error) {
-            console.log('test upload to database error')
-            throw new Error(error)
-        }
-        else {
-            const test = await Test.create({ problem: problem, file: file._id })
-            await Problem.findOneAndUpdate({ name: problem }, { '$push': { 'tests': test._id } })
-            console.log('test for problem', problem, 'saved')
-        }
-    })
+
+    const test = await Test.create({ problem: problem, file: file._id })
+    await Problem.findOneAndUpdate({ name: problem }, { '$push': { 'tests': test._id } })
+    console.log('test for problem', problem, 'saved')
+}
+
+async function toFile(testId, path) {
+    const test = await Test.findOne({ _id: testId })
+    await File.toFile(test.file, path)
 }
 
 async function getTestStream(testId) {
-    const TestFile = await createModel({
-        modelName: 'TestFiles'
-    })
-    const fileId = Test.findOne(testId).file
-    return TestFile.read({ fileId })
+    const test = await Test.findOne({ _id: testId })
+    return File.getStream(test.file)
 }
 
 async function remTest(testId) {
-    testObj = Test.findOne( { _id: testID } )
+    testObj = await Test.findOne( { _id: testId } )
     const fileId = testObj.file
-    const TestFile = await createModel({
-        modelName: 'TestFiles'
-    })
-    TestFile.unlink({ fileId })
-    Problem.findOneAndUpdate( { name: testObj.problem }, { $pull: { tests: testId } })
-    Test.deleteOne( { _id: testId } )
+
+    await File.unlink({ fileId })
+    await Problem.findOneAndUpdate( { name: testObj.problem }, { $pull: { tests: testId } })
+    await Test.deleteOne( { _id: testId } )
 }
 
-module.exports = { Test, insertTest, remTest, getTestStream }
+module.exports = { Test, insertTest, remTest, toFile, getTestStream }
